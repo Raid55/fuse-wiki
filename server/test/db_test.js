cd25f58cf3c60ab01de72dfe08915ca400fb42fe
Nicholas Boutboul <boutboulnicholas@gmail.com>
2018-04-23 02:48:15 -0700

    new

--- server/test/db_test.js
+++ server/test/db_test.js
@@ -4,7 +4,7 @@ const redirects = require('../models/redirects.js');
 const pages = require('../models/pages.js')
 const Sequelize = require('sequelize');
 
-db = Db(Sequelize, "../fuse.sqlite", links, pages, redirects)
+db = new Db(Sequelize, "../fuse.sqlite", links, pages, redirects)
 
 db.sequelize.sync().then(() => {
     test = db.test(["20845297"])
