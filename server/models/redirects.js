e0b66081eb595cdd3c4f186e8f0cf1d4fc75588b
Nicholas Boutboul <boutboulnicholas@gmail.com>
2018-04-23 03:14:13 -0700

    lowercase redir

--- server/models/redirects.js
+++ server/models/redirects.js
@@ -1,5 +1,5 @@
 module.exports = function(sequelize, DataTypes) {
-    return sequelize.define("Redirects", {
+    return sequelize.define("redirects", {
         source_id: {
             type: DataTypes.INTEGER,
             primaryKey: true
