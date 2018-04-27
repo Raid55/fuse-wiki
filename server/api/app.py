cf8ea19f13d14f1b771aae4031f4ff7ffca16738
Nicholas Boutboul <boutboulnicholas@gmail.com>
2018-04-27 09:35:50 -0700

    ready or testing

--- server/api/app.py
+++ server/api/app.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+import socket
 from models import db
 from os import getenv
 from api import api_v1
@@ -19,10 +20,16 @@ def not_found(err):
     return make_response(jsonify({"error": "There is nothing here..."}), 404)
 
 if __name__ == "__main__":
-    app_host = getenv('API_HOST')
+    try:
+        host_name = socket.gethostname()
+        app_host = socket.gethostbyname(host_name)
+    except:
+        app_host = getenv('API_HOST')
+        if app_host is None:
+            app_host = '0.0.0.0'
+    
     app_port = getenv('API_PORT')
-    if app_host is None:
-        app_host = '0.0.0.0'
     if app_port is None:
         app_port = 8080
+    
     app.run(host=app_host, port=int(app_port))
--- server/api/v1/generate_link.py
+++ server/api/v1/generate_link.py
@@ -1,6 +1,9 @@
 from models import db
 from api.v1 import api_v1
 from flask import jsonify, abort, request
+from helpers.b_dir_earch import findTheWikiConnection
+from helpers.wikipedia_api import wiki_search_id
+from models import db
 
 
 @app_views.route('/generate_link', methods=['PUT'])
@@ -10,5 +13,15 @@ def generate_link():
     if data is None:
         return jsonify({'error':'Not in JSON format'}), 400
     
-    if not data['raw_source_title'] or not data['raw_target_title']:
-        
+    if not data['raw_source_title']:
+        return jsonify({'error': 'Missing name'}), 400
+    if not data['raw_target_title']:
+        return jsonify({'error': 'Missing raw_target_title'}), 400
+
+    return jsonify({
+            'result': findTheWikiConnection(
+                db, 
+                wiki_search_id(data['raw_source_title']),
+                wiki_search_id(data['raw_target_title'])
+            )
+        }), 200
