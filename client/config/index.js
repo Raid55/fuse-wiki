f2eefafca39c0db52d30e758a431fc95285de052
Nicholas Boutboul <boutboulnicholas@gmail.com>
2018-05-01 16:00:39 -0700

    removed app server to start fresh

--- client/config/index.js
+++
@@ -1,6 +0,0 @@
-require('dotenv').config();
-
-module.exports = {
-  PORT: process.env.PORT || process.env.DEV_PORT || 3000,
-  ADDRESS: process.env.ADDRESS || process.env.DEV_ADDRESS || "localhost"
-};
