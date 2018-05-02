c356d6158e7fc6ca001c48d8158e35c262d4f65e
JacobBHartman <209@holbertonschool.com>
2018-05-02 02:21:27 +0000

    create 0 degrees and eror check

--- server/helpers/b_dir_earch.py
+++ server/helpers/b_dir_earch.py
@@ -11,5 +11,19 @@ def bi_dir_earch(db, source_id, target_id):
         paths of Wikipedia articles that link a source article and a
         target article. This is v2
     '''
+    ##### ERROR CHECKING #####
+    if source_id is None or target_id is None:
+        return "Error: There was a non-existent source or target article"
 
+    ##### DECLARE PERSISTENT OBJECTS #####
+    source_tree = "SQLite call"
+    target_tree = "SQLite call"
+    source_depth = 0
+    target_depth = 0
+
+    ##### ZERO DEGREES OF SEPARATION #####
+    if source_id == target_id:
+        return [source_id, target_id]
+
+    ##### ONE DEGREE OF SEPARATION #####
     
