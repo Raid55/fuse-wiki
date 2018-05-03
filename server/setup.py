890c93d6517b0d0080a8d177ca4d01929d9c55be
Nicholas Boutboul <boutboulnicholas@gmail.com>
2018-05-02 19:49:50 -0700

    testing wikipedia thing

--- server/setup.py
+++ server/setup.py
@@ -7,7 +7,9 @@ requires = [
     'Flask',
     'sqlite3',
     'requests',
-    'flask_cors'
+    'flask_cors',
+    'gunicorn',
+    'supervisor'
 ]
 
 
@@ -21,10 +23,3 @@ setup(
     zip_safe=False,
     install_requires=requires
 )
-
-flask-cors == 3.0.3
-google-cloud-logging == 1.5.0
-google-compute-engine == 2.7.6
-gunicorn == 19.7.1
-requests == 2.18.4
-supervisor == 3.3.4
