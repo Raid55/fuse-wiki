88e693671f8f8c08218b433ec541c09b7c7425cf
Nicholas Boutboul <boutboulnicholas@gmail.com>
2018-05-09 09:49:56 -0700

    logging for gunicorn

--- server/api/app.py
+++ server/api/app.py
@@ -4,7 +4,7 @@ import socket
 from os import getenv
 from api.v1 import api_v1
 from flask import Flask, make_response, jsonify
-from raven.contrib.flask import Sentry
+# from raven.contrib.flask import Sentry
 from flask_cors import CORS
 
 
@@ -13,7 +13,7 @@ CORS(app)
 
 app.register_blueprint(api_v1, url_prefix='/v1')
 
-sentry = Sentry(app, dsn=getenv('SENTRY_DSN'))
+# sentry = Sentry(app, dsn=getenv('SENTRY_DSN'))
 
 # @app.teardown_appcontext
 # def close_method(exception):
